529a2b2ac53e0955327c5f201cb3998918953cc8
Joao Cicero <jc_ifsp@hotmail.com>
2013-01-22 23:53:19 -0200

    Comit Inicial

---
+++ SystemFunilaria/Cad_Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemFunilaria
+{
+    public partial class Cad_Cliente : Form
+    {
+        public Cad_Cliente()
+        {
+            InitializeComponent();
+        }
+    }
+}
